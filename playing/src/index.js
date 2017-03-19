dfed259d943716c55eec2e479489aa57d26b3679
ablackledge <a.blackledge@building-blocks.com>
2017-03-19 16:24:21 +0000

    adding routing

--- playing/src/index.js
+++ playing/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+
 import App from './App';
+import NotFound from "./NotFound";
+import Test from "./Test";
+
 import './index.css';
 
+const Root = () => {
+    return(
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/test/" component={Test} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
+    )
+}
+
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
